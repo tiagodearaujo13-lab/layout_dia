c6c34ad6e0a583e80f403a95207d448e93c01c60
Tiago <o-seu-email-do-github@exemplo.com>
2026-01-28 11:14:23 +0000

    updade task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document - addEventListener('DOMContentLoaded', () => {
+
+  const menuButton = document.querySelector('.header__menu-button');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  if (menuButton) {
+    menuButton.addEventListener('click', () => {
+      document.body.classList.toggle('is-menu-open');
+    });
+  }
+});
